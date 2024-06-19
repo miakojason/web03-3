1aac99d58def95c534860095057374b59df200c4
miakojason <miakojason@gmail.com>
2024-06-19 12:01:13 +0800

    完成booking css

--- api/booking.php
+++ api/booking.php
@@ -1,7 +1,33 @@
-<?php include_once "php"; ?>
-
+<?php include_once "./db.php"; ?>
+<style>
+    #room{
+        background-image: url("./icon/03D04.png");
+        /* background-position: center; */
+        /* background-repeat:no-repeat; */
+        width: 540px;
+        height: 370px;
+        margin: auto;
+        box-sizing: border-box;
+        /* 上右下左 */
+        padding:19px 112px 0 112px ;
+    }
+    .seats{
+        display: flex;
+        flex-wrap: wrap;
+    }
+    .seat{
+        width: 63px;
+        height: 85px;
+        position: relative;
+    }
+   .chk{
+    position: absolute;
+    right: 2px;
+    bottom:2px;
+   }
+</style>
 <div id="room">
-    <div class="seat">
+    <div class="seats">
         <?php
         for ($i = 0; $i < 20; $i++) {
         ?>
@@ -18,4 +44,13 @@
         }
         ?>
     </div>
+</div>
+<div id="info">
+    <div>您選擇的電影是:</div>
+    <div>您選擇的時刻是:</div>
+    <div>您已經勾選<span id="tickes">0</span>張票，最多可以購買四張票</div>
+<div>
+    <button onclick="$('#select').show();$('#booking').hide()">上一步</button>
+    <button onclick="checkout()">訂購</button>
+</div>
 </div>
